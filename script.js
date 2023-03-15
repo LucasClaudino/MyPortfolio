85c06caaa0302064520e62e03d7b36709221394b
LucasClaudino <lucas.claudinod@gmail.com>
2023-03-14 22:19:48 -0300

    finish projects and other visual adjustments

---
+++ script.js
@@ -0,0 +1,26 @@
+let totalSlides = document.querySelectorAll('.slide').length;
+let currentSlide = 0;
+
+
+document.querySelector('.slider-controls').style.height = `${document.querySelector('.slider').clientHeight}px`;
+
+function goPrev() {
+    currentSlide--;
+    if(currentSlide < 0) {
+        currentSlide = totalSlides - 1;
+    }
+    updateMargin();
+}
+function goNext() {
+    currentSlide++;
+    if(currentSlide > (totalSlides - 1)) {
+        currentSlide = 0;
+    }
+    updateMargin();
+}
+function updateMargin() {
+    let newMargin = (currentSlide * document.querySelector('.slide').clientWidth);
+    document.querySelector('.slider').style.marginLeft = `-${newMargin}px`;
+}
+
+setInterval(goNext, 10000);
