606b94a6cdb102871021e89c2e8200daa23e9250
LucasClaudino <lucas.claudinod@gmail.com>
2023-04-13 13:03:14 -0300

    toggle menu and scroll

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+//Menu
+function selectMenu(n) {
+    let menus = document.querySelectorAll('nav ul li');
+    for(i=0; i<menus.length; i++) {
+        menus[i].classList.remove('selected');
+    }
+    menus[n].classList.add('selected');
+};
+
+function toggleMenu() {
+    let nav = document.querySelector('nav');
+    let menuImg = document.querySelector('.menu-opener');
+    if(nav.classList.contains('show')) {
+        nav.classList.remove('show');
+        menuImg.style.backgroundImage = ('url(/images/menu.png)');
+    } else {
+        nav.classList.add('show');
+        menuImg.style.backgroundImage = ('url(/images/close.png)');
+    };
+};
+
+let sections = document.querySelectorAll('section');
+let menuList = document.querySelectorAll('nav ul li');
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                let id = entry.target.id;
+                for(i=0; i<menuList.length; i++) {
+                    menuList[i].classList.remove('selected');
+                    if(menuList[i].classList.contains(id)) {
+                        menuList[i].classList.add('selected')
+                    }
+                }
+            }
+        })
+    },
+    {
+        threshold: 0.7,
+    }
+);
+
+sections.forEach(element => {
+    observer.observe(element);
+});
+
+
 // Slides
 
 let totalSlides = document.querySelectorAll('.slide').length;
@@ -24,3 +71,4 @@ function updateMargin() {
     let newMargin = (currentSlide * document.querySelector('.slide').clientWidth);
     document.querySelector('.slider').style.marginLeft = `-${newMargin}px`;
 };
+
